970ea7336eccc110a171f8afb5cb71769c164e29
Ivan Savchuk <savchuk.ivan.1@gmail.com>
2022-12-02 16:58:43 +0200

    fixed pytz

--- get_time.py
+++ get_time.py
@@ -1,14 +1,14 @@
-import pytz
+from pytz import timezone
 from datetime import datetime
 
 def day_of_week():
-    return (int(datetime.now(tz=pytz.timezone('Europe/Kiev')).strftime('%w'))-1)%7
+    return (int(datetime.now(tz=timezone('Europe/Kiev')).strftime('%w'))-1)%7
 
 def get_week():
-    return int(datetime.now(tz=pytz.timezone('Europe/Kiev')).strftime('%W')) % 2
+    return int(datetime.now(tz=timezone('Europe/Kiev')).strftime('%W')) % 2
 
 def tomorrow():
-    return int(datetime.now(tz=pytz.timezone('Europe/Kiev')).strftime('%w'))
+    return int(datetime.now(tz=timezone('Europe/Kiev')).strftime('%w'))
 
 if __name__ == '__main__':
     print(day_of_week())
